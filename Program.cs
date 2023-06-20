8fe1c4cdd36abd684fb4afebee1749b48e2c48e6
ALEX K. KEMBOI <AKEMBOI@fintech-group.com>
2023-06-20 13:00:04 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+class Program
+{
+    static void Main(string[] args)
+    {
+            // Create a new hash table
+            Hashtable hashtable = new Hashtable();
+
+            // Add key-value pairs to the hash table
+            hashtable.Add("apple", "A fruit");
+            hashtable.Add("car", "A vehicle");
+            hashtable.Add("sun", "A star");
+
+            // Access values by key
+            string value1 = (string)hashtable["apple"];
+            string value2 = (string)hashtable["car"];
+            string value3 = (string)hashtable["sun"];
+
+            Console.WriteLine("Value for key 'apple': " + value1);
+            Console.WriteLine("Value for key 'car': " + value2);
+            Console.WriteLine("Value for key 'sun': " + value3);
+
+            // Check if a key exists
+            bool containsKey = hashtable.ContainsKey("apple");
+            Console.WriteLine("Contains key 'apple': " + containsKey);
+
+            // Remove a key-value pair
+            hashtable.Remove("car");
+
+            // Iterate over the keys and values
+            foreach (DictionaryEntry entry in hashtable)
+            {
+                string key = (string)entry.Key;
+                string value = (string)entry.Value;
+                Console.WriteLine("Key: " + key + ", Value: " + value);
+            }
+        
+    }
+}
